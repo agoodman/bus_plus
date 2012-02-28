457525963ae521ff37e910aa6c8084353cd2b020
Aubrey <aubrey.goodman@gmail.com>
2012-02-28 12:18:03 -0500

    removed presence validation on start and end time from segment

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -3,7 +3,7 @@ class Segment < ActiveRecord::Base
   belongs_to :vehicle
   belongs_to :passenger
   
-  validates_presence_of :passenger_id, :start_latitude, :start_longitude, :start_time, :end_latitude, :end_longitude, :end_time
+  validates_presence_of :passenger_id, :start_latitude, :start_longitude, :end_latitude, :end_longitude
   validates_numericality_of :start_latitude, :start_longitude, :end_latitude, :end_longitude
   
 end
