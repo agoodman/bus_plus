3275845abf17a2b5203295755e7c5575f54456aa
Aubrey <aubrey.goodman@gmail.com>
2012-02-28 12:17:28 -0500

    added dependent destroy to passenger.segment

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,6 @@
 class Passenger < ActiveRecord::Base
 
-  has_one :segment
+  has_one :segment, dependent: :destroy
   
   validates_presence_of :latitude, :longitude
   validates_numericality_of :latitude, :longitude
