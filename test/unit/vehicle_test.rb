967b76371f71c1b4800624573426944dbd231db4
Aubrey <aubrey.goodman@gmail.com>
2012-11-26 00:42:36 -0500

    updated vehicle model, view, controller, and tests to incorporate candidates

--- test/unit/vehicle_test.rb
+++ test/unit/vehicle_test.rb
@@ -4,12 +4,32 @@ require 'shoulda'
 class VehicleTest < ActiveSupport::TestCase
 
   should belong_to :driver
-  should have_many :segments
   should have_many :passengers
+  should have_many :candidates
   
   should validate_presence_of :latitude
   should validate_presence_of :longitude
   should validate_numericality_of :latitude
   should validate_numericality_of :longitude
+
+  context "a vehicle with one seat available on decrement" do
+    setup do
+      @vehicle = FactoryGirl.create(:vehicle, seats_available: 1)
+      @vehicle.decrement_seat_count!
+    end
+    should "have no seats available" do
+      assert_equal(@vehicle.seats_available, 0)
+    end
+  end
+  
+  context "a vehicle with no seats available on increment" do
+    setup do
+      @vehicle = FactoryGirl.create(:vehicle, seats_available: 0)
+      @vehicle.increment_seat_count!
+    end
+    should "have one seat available" do
+      assert_equal(@vehicle.seats_available, 1)
+    end
+  end
   
 end
