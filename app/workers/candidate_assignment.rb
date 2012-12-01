287a96e4ca4659ab9d7174659e54b9efb2a0cfd9
Aubrey <aubrey.goodman@gmail.com>
2012-12-01 15:04:01 -0500

    added grocer integration for candidate bid request and assignment

--- app/workers/candidate_assignment.rb
+++ app/workers/candidate_assignment.rb
@@ -1,8 +1,41 @@
 class CandidateAssignment
 
   def self.send_assignment(passenger_id,vehicle_id)
-    puts "TODO: send APNs assignment to passenger #{passenger_id} and vehicle #{vehicle_id}"
-    # TODO grocer integration
+    puts "send APNs assignment to passenger #{passenger_id} and vehicle #{vehicle_id}"
+    passenger = Passenger.find(passenger_id)
+    vehicle = Vehicle.find(vehicle_id)
+    send_passenger_assignment_notification(passenger,vehicle)
+    send_vehicle_assignment_notification(passenger,vehicle)
+  end
+
+  def self.send_passenger_assignment_notification(passenger,vehicle)
+    pusher = Grocer.pusher(
+      certificate: "config/apns/passenger-development.pem",
+      gateway: "gateway.sandbox.push.apple.com"
+    )
+      
+    payload = { vehicle_id: vehicle.id }
+    notification = Grocer::Notification.new(
+      device_token: passenger.token,
+      custom: payload
+    )
+
+    pusher.push(notification)
+  end
+  
+  def send_vehicle_assignment_notification(passenger,vehicle)
+    pusher = Grocer.pusher(
+      certificate: "config/apns/driver-development.pem",
+      gateway: "gateway.sandbox.push.apple.com"
+    )
+      
+    payload = { passenger_id: passenger.id }
+    notification = Grocer::Notification.new(
+      device_token: vehicle.token,
+      custom: payload
+    )
+
+    pusher.push(notification)
   end
   
 end
--- app/workers/candidate_bid_request.rb
+++ app/workers/candidate_bid_request.rb
@@ -1,10 +1,24 @@
 class CandidateBidRequest
   
   def self.send_bid_request(candidate_id)
-    puts "TODO: send APNs bid request to candidate #{candidate_id}"
-    # TODO grocer integration
-    # for now, just schedule a callback 5sec later
-    CandidateBidRequestStub.delay(run_at: 5.seconds.from_now).callback(candidate_id)
+    puts "send APNs bid request to candidate #{candidate_id}"
+    
+    send_bid_request Candidate.find(candidate_id)
+  end
+  
+  def self.send_bid_request(candidate)
+    pusher = Grocer.pusher(
+      certificate: "config/apns/driver-development.pem",
+      gateway: "gateway.sandbox.push.apple.com"
+    )
+      
+    payload = { candidate_id: candidate_id }
+    notification = Grocer::Notification.new(
+      device_token: candidate.vehicle.token,
+      custom: payload
+    )
+    
+    pusher.push(notification)
   end
   
 end
