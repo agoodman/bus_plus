fb27c8e487ce0bbc4ef6bfbc492392f3cfa8fc67
Aubrey <aubrey.goodman@gmail.com>
2012-12-01 15:30:52 -0500

    shifted candidate lookup into send method

--- app/workers/candidate_bid_request.rb
+++ app/workers/candidate_bid_request.rb
@@ -3,16 +3,17 @@ class CandidateBidRequest
   def self.send_bid_request(candidate_id)
     puts "send APNs bid request to candidate #{candidate_id}"
     
-    send_bid_request Candidate.find(candidate_id)
+    send_bid_request candidate_id
   end
   
-  def self.send_bid_request(candidate)
+  def self.send_bid_request(candidate_id)
     pusher = Grocer.pusher(
       certificate: "config/apns/driver-development.pem",
       gateway: "gateway.sandbox.push.apple.com"
     )
-      
-    payload = { candidate_id: candidate.id }
+    
+    candidate = Candidate.find(candidate_id)
+    payload = { candidate_id: candidate_id }
     notification = Grocer::Notification.new(
       device_token: candidate.vehicle.token,
       custom: payload
