b8af2f1cea4109a2cfce5c5f5eceb2968e8d9c49
Aubrey <aubrey.goodman@gmail.com>
2012-12-09 19:34:17 -0500

    helps if we pass in the right param

--- app/workers/candidate_search.rb
+++ app/workers/candidate_search.rb
@@ -5,7 +5,7 @@ class CandidateSearch
     nearby_vehicles = Vehicle.on_duty.with_vacancy.near(passenger.start_latitude, passenger.start_longitude).limit(20)
     if nearby_vehicles.empty?
       puts "no nearby vehicles found for passenger #{passenger_id}"
-      PassengerReject.delay.reject(passenger_id)
+      PassengerReject.delay.reject(passenger.token)
     else
       for vehicle in nearby_vehicles
         puts "found candidate vehicle #{vehicle.id}"
